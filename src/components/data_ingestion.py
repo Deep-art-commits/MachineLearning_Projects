5d25f37d46fb94c9bf842b1a3c88709f185396da
Deep-art-commits <dilpreet.singh12@outlook.com>
2024-06-15 09:37:36 +0530

    15/06/24

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from src.exception import custom_exception
+from src.logger import logging
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class Data_ingestion_config:
+    raw_data_path:str=os.path.join('artifacts',"raw_data.csv")
+    train_data_path:str=os.path.join('artifacts',"train.csv")
+    test_data_path:str=os.path.join('artifacts',"test.csv")
+
+class data_ingestion:
+    def __init__(self) :
+        self.ingestion_config=Data_ingestion_config()
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info("Reading the dataset as a Pandas Dataframe ")
+             # creating directories
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+           
+            logging.info("Train_test_split started ")
+            # splitting data into test and train 
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=10)
+            # Storing train and test data in respective csv files 
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Ingestion of Data is complete ")
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+            
+        except Exception as e :
+            raise custom_exception(e,sys)
+
+if __name__=="__main__" :
+    obj=   data_ingestion() 
+    obj.initiate_data_ingestion()
+   
